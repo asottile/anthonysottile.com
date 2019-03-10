215540bae31e2c872b5145c80187bf384bb42c96
Anthony Sottile <asottile@umich.edu>
2019-03-09 21:26:26 -0800

    Convert to python

--- includes/404.body.php
+++
@@ -1,7 +0,0 @@
-<h2 class="header">
-    404 - Oops we don't have that page!
-</h2>
-<p>
-    The Requested Page (<?=$_SERVER['REQUEST_URI']?>) Was Not Found On This Server!<br /><br />
-    <a href="<?=url('index')?>">Home</a>
-</p>
